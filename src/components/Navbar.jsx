df3dbfcd7612a315ab3ca5786ef9f4bb32fec423
Dhanalakshmi M <145017761+DhanalakshmiM22004@users.noreply.github.com>
2025-07-12 13:26:56 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
 
       <ul className={menuOpen ? 'active' : ''}>
         {[
-          { href: '/Portfolio', label: 'Home' },
+          { href: '/', label: 'Home' },
           { href: '#about', label: 'About' },
           { href: '#Skills', label: 'Skill' },
           { href: '#Project', label: 'Projects' },
